f4c4292cb3ad805a934348cc5df46df4942a0b38
Anas <amanabbasi0630@gmail.com>
2022-09-28 12:10:55 +0530

    build UI for dapp

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from "../components/Header"
+import Main from "../components/Main"
+
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+}
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Web3 Uniswap</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={style.wrapper}>
+      <Header />
+      <Main />
+      <p>Hum</p>
+      {/* <Main /> */}
+      {/* <TransactionHistory /> */}
+    </div>
 </div>
   )
 }
